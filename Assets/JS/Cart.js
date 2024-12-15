f7e02c8495ea69d97fbd3a2313d3feaa4f0c2f5e
Claudia M. <cmelladev@gmail.com>
2024-12-15 00:43:38 -0300

    cambios2

--- Assets/JS/Cart.js
+++ Assets/JS/Cart.js
@@ -3,33 +3,33 @@ let ShoppingCart = document.getElementById('shopping-cart');
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-let calculation =()=>{
+// Actualiza el ícono del carrito con la cantidad total de ítems
+let calculation = () => {
     let cartIcon = document.getElementById("cartamount");
-    cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0);    
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);    
 };
 
 calculation();
 
-let generateCartItems = ()=>{
-    if(basket.length !==0){
-        return ShoppingCart.innerHTML = basket.map((x)=>{
-            return '
-            <div class="cart-item"></div>
-        
-            '
-        })
+// Genera los elementos del carrito
+let generateCartItems = () => {
+    if (basket.length !== 0) {
+        ShoppingCart.innerHTML = basket.map((x) => {
+            return `
+            <div class="cart-item">
+                <h4>Item: ${x.item}</h4>
+            </div>
+            `;
+        }).join(""); // Unimos los elementos para que no aparezcan separados por comas
+    } else {
+        ShoppingCart.innerHTML = '';
+        label.innerHTML = `
+            <h2>Tu carrito está vacío</h2>
+            <a href="index.html">
+                <button class="HomeBtn">Volver al Inicio</button>
+            </a>
+        `;
     }
-
-    else{
-     ShoppingCart.innerHTML = '';
-     label.innerHTML = '
-     <h2>Cart is empty</h2>
-     <a href="index.html">
-        <button class="HomeBtn">Back to Home</button>
-    
-     </a>
-     '
-    }
-}
+};
 
 generateCartItems();
--- Assets/JS/Data.js
+++ Assets/JS/Data.js
@@ -1,24 +1,24 @@
 let shopItemsData = [{
-    id: "dom",
-    name: "Camisa casual",
-    price: 45,
+    id: "Item1",
+    name: "Termo",
+    price: 6500,
     img: "Assets/Img/img-1.jpg"
 },
 {
-    id: "re",
-    name: "Camisa formal",
-    price: 100,
+    id: "Item2",
+    name: "Reloj",
+    price: 3000,
     img: "Assets/Img/img-2.jpg"
 },
 {
-    id: "mi",
-    name: "Polera",
-    price: 25,
+    id: "Item3",
+    name: "Porta tarjeta",
+    price: 2000,
     img: "Assets/Img/img-3.jpg"
 },
 {
-    id: "fa",
-    name: "Traje formal",
-    price: 300,
+    id: "Item4",
+    name: "Figura aleatoria",
+    price: 2500,
     img: "Assets/Img/img-4.jpg"
 }]
