b52f744744b22708cf7935946c46a41fb8baa0fa
Claudia M. <cmelladev@gmail.com>
2024-12-15 19:26:55 -0300

    Ultimo cambio

--- Assets/JS/Cart.js
+++ Assets/JS/Cart.js
@@ -12,15 +12,39 @@ let calculation = () => {
 calculation();
 
 // Genera los elementos del carrito
+
 let generateCartItems = () => {
     if (basket.length !== 0) {
-        ShoppingCart.innerHTML = basket.map((x) => {
+        return ShoppingCart.innerHTML = basket.map((x) => {
+            let{id, item }= x;
+            let search = shopItemsData.find((y)=>y.id === id) || [];
+            let {img, name, price} = search
+
             return `
             <div class="cart-item">
-                <h4>Item: ${x.item}</h4>
+                <img width="100" src=${img} alt"" />
+                <div class="details">
+
+                <div class="title-price-x">
+                    <h4 class="title-price">
+                        <p>${name}</p>
+                        <p class="cart-item-price">$ ${price}</p>
+                    </h4>
+                    <i onclick="removeItem(${id})" class="bi bi-x"></i>
+                </div>
+
+                <div class="buttons">
+                        <i onclick="decrement(${id})" class="bi bi-patch-minus"></i>
+                        <div id=${id} class="quantity"> ${item} </div>
+                        <i onclick="increment(${id})" class="bi bi-patch-plus"></i>
+                </div>
+
+                <h3>$ ${item * search.price}</h3>
+                </div>
             </div>
             `;
-        }).join(""); // Unimos los elementos para que no aparezcan separados por comas
+        }).join("");
+
     } else {
         ShoppingCart.innerHTML = '';
         label.innerHTML = `
@@ -33,3 +57,91 @@ let generateCartItems = () => {
 };
 
 generateCartItems();
+
+let increment = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+    
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    }
+
+    else{
+        search.item += 1;
+    }
+   
+    generateCartItems();
+    update(selectedItem.id);
+    
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+    
+    if(search === undefined) return;
+
+    else if(search.item === 0) return;
+
+    else{
+        search.item -= 1;
+    }
+   
+    update(selectedItem.id);
+
+    basket = basket.filter((x)=>x.item !== 0);
+    generateCartItems();
+  
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id)=>{
+    let search = basket.find((x)=>x.id === id)
+    //console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+    TotalAmount();
+};
+
+let removeItem = (id)=>{
+    let selectedItem = id;
+    //console.log (selectedItem.id);
+    basket = basket.filter((x)=>x.id !== selectedItem.id);
+    generateCartItems();
+    TotalAmount();
+    calculation();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let TotalAmount = ()=>{
+    if(basket.length !==0) {
+        let amount = basket.map((x)=>{
+            let {item, id} = x;
+            let search = shopItemsData.find((y)=>y.id === id) || [];
+            return item * search.price;
+        })
+        .reduce((x,y)=>x+y, 0);
+        
+        label.innerHTML = `
+            <h2>Total $ ${amount}</h2>
+            <button class="checkout">Pagar Artículos</button>
+            <button onclick="clearCart()" class="removeAll">Borrar Todo</button>
+        `
+    }
+
+    else return
+};
+
+let clearCart = () => {
+    basket = [];
+    generateCartItems();
+    calculation();
+    TotalAmount();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+TotalAmount();
--- Assets/JS/Data.js
+++ Assets/JS/Data.js
@@ -21,4 +21,52 @@ let shopItemsData = [{
     name: "Figura aleatoria",
     price: 2500,
     img: "Assets/Img/img-4.jpg"
+},
+{
+    id: "Item5",
+    name: "Contenedor",
+    price: 7500,
+    img: "Assets/Img/img-5.jpg"
+},
+{
+    id: "Item6",
+    name: "Llavero",
+    price: 2000,
+    img: "Assets/Img/img-6.jpg"
+},
+{
+    id: "Item7",
+    name: "Calcetines",
+    price: 2500,
+    img: "Assets/Img/img-7.jpg"
+},
+{
+    id: "Item8",
+    name: "Mini Carpeta",
+    price: 5000,
+    img: "Assets/Img/img-8.jpg"
+},
+{
+    id: "Item9",
+    name: "Notas Adhesivas",
+    price: 1500,
+    img: "Assets/Img/img-9.jpg"
+},
+{
+    id: "Item10",
+    name: "Estuche",
+    price: 3500,
+    img: "Assets/Img/img-10.jpg"
+},
+{
+    id: "Item11",
+    name: "Antifaz",
+    price: 3000,
+    img: "Assets/Img/img-11.jpg"
+},
+{
+    id: "Item12",
+    name: "Peluche",
+    price: 6000,
+    img: "Assets/Img/img-12.jpg"
 }]
